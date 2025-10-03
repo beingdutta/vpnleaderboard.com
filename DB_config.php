e097b8b9395be64c4f3f0f184db2c12cdfe3676d
Aritra Dutta <aritra.duttas18@gmail.com>
2025-10-04 00:51:54 +0530

    ignore updated

--- DB_config.php
+++ DB_config.php
@@ -1,6 +1,6 @@
 <?php
 $DB_HOST = 'localhost'; 
-$DB_NAME = 'u628985654_vpnboard'; 
-$DB_USER = 'u628985654_vpnuser'; 
-$DB_PASS = 'Vpn18##@@@'; 
+$DB_NAME = 'vpnboard'; 
+$DB_USER = 'root'; 
+$DB_PASS = ''; 
 ?>
