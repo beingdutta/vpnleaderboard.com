e261cfe20e356f4eee1f2a49b46ed012de0c46be
Aritra Dutta <aritra.duttas18@gmail.com>
2025-10-04 00:44:13 +0530

    DB Connection Error Verbosed

--- db.php
+++ db.php
@@ -10,12 +10,11 @@ $options = [
 ];
 
 try {
-  $pdo = new PDO(
-    "mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8mb4",
-    $DB_USER,
-    $DB_PASS,
-    $options
-  );
+    $dsn = "mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8mb4";
+    $pdo = new PDO($dsn, $DB_USER, $DB_PASS, $options);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
 } catch (Exception $e) {
   http_response_code(500);
   echo "Database connection error.";
